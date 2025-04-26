97e4e8fe7a5e39fa132fbffa5bb19005f0e7c1f4
John <139019965+JBigode@users.noreply.github.com>
2025-04-26 13:30:49 +0000

    Aventureiro Concluido

--- JogoXadrez.c
+++ JogoXadrez.c
@@ -2,7 +2,7 @@
 
 int main() {
 
-    int i = 1;
+    int i = 1, cavaloMov = 1;
     
     // Mover o Bispo 5 casas na diagonal.
     printf("### Movimentacao do Bispo ###\n");
@@ -25,11 +25,20 @@ int main() {
     } while (i <= 5);
 
     // Mover a Torre 5 casas para a direita.
-    printf("\n\n### Movimentacao da torre ###\n");
+    printf("\n\n### Movimentacao da Torre ###\n");
     for (i = 1; i <= 5; i++)
     {
         printf("Direita %d\n", i);
     }
-    
+
+    // Mover o cavalo 2 casas para baixo e 1 para a esquerda.
+    printf("\n\n### Movimentacao do Cavalo ###\n");
+    while (cavaloMov--)
+    {
+     for (int i = 1; i < 3; i++){
+        printf("Baixo %d\n", i);
+    }
+     printf("Esquerda %d\n", cavaloMov +1);
+    }
     return 0;
 }
